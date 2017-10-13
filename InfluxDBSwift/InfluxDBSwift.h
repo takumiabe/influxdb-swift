581a0b1a7164239ab50ab8ca800fb71d1d9ddfca
Takumi Abe <abe@engraphia.com>
2017-10-13 18:13:50 +0900

    setup XCode multiplatform project

---
+++ InfluxDBSwift/InfluxDBSwift.h
@@ -0,0 +1,18 @@
+//
+//  InfluxDBSwift.h
+//  InfluxDBSwift
+//
+//  Created by takumiabe on 2017/10/13.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for InfluxDBSwift.
+FOUNDATION_EXPORT double InfluxDBSwiftVersionNumber;
+
+//! Project version string for InfluxDBSwift.
+FOUNDATION_EXPORT const unsigned char InfluxDBSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InfluxDBSwift/PublicHeader.h>
+
+
